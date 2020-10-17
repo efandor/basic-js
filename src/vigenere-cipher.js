a5cf471072e0f24dde828c4ed921c285bb88bac6
efandor <firstdir@gmail.com>
2020-10-17 14:01:11 +0300

    feat: add solution to vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(isTrue) {
+        if (isTrue === undefined || isTrue === true) {
+            this.isTrue = true;
+        } else {
+            this.isTrue = false;
+        }
+    }
+
+    encrypt(message, key) {
+        const messageArray = message.toUpperCase().split('');
+        const arrayKey = key.toUpperCase().split('');
+        let j = 0;
+        let digitCode = 0;
+        for (let i = 0; i < messageArray.length; i++) {
+            if (messageArray[i].charCodeAt(0) > 64 && messageArray[i].charCodeAt(0) < 91) {
+              digitCode = messageArray[i].charCodeAt(0) + arrayKey[j].charCodeAt(0) - 65;
+              if (digitCode > 90) digitCode -= 26;
+              messageArray[i] = String.fromCharCode(digitCode);
+              if (j < arrayKey.length-1) {
+                j++;
+              } else j = 0;
+            } else continue;
+        }
+        return this.isTrue ? messageArray.join('') : messageArray.reverse().join('');
+    }
+
+    decrypt(message, key) {
+        const messageArray = message.split('');
+        const arrayKey = key.toUpperCase().split('');
+        let j = 0;
+        let digitCode;
+        for (let i = 0; i < messageArray.length; i++) {
+            if (messageArray[i].charCodeAt(0) < 65 || messageArray[i].charCodeAt(0) > 90) {
+                continue;
+            }
+            digitCode = messageArray[i].charCodeAt(0) - arrayKey[j].charCodeAt(0) + 65;
+            if (digitCode < 65) digitCode += 26;
+            messageArray[i] = String.fromCharCode(digitCode);
+            if (j < arrayKey.length-1) {
+              j++;
+            } else j = 0;
+      }
+        return this.isTrue ? messageArray.join('') : messageArray.reverse().join('');
+    }
 }
 
 module.exports = VigenereCipheringMachine;
